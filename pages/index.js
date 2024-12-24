193170732b9a02124b78c6395e3f9934a658a8cc
Ozgur Demir <ozgur.d@freelancer.run>
2024-12-24 12:54:41 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -6,9 +6,7 @@ export default function Home() {
 
     const calculateDates = () => {
         const demir = new Date('2024-11-02');
-        const coskun = new Date('2024-09-19');
         const mert = new Date('2025-01-20');
-        // const elgormus = new Date('2024-06-04');
         const elgormus = null;
         const tanya = new Date('2024-03-05');
         const today = new Date();
@@ -34,11 +32,10 @@ export default function Home() {
         };
 
         return [
-            {name: 'MERT', ...calculateDaysLeft(mert)},
-            {name: 'DEMIR', ...calculateDaysLeft(demir)},
-            {name: 'COSKUN', ...calculateDaysLeft(coskun)},
-            {name: 'ELGORMUS', ...calculateDaysLeft(elgormus)},
-            {name: 'ALMIRA BIRTHDAY', ...calculateDaysLeft(tanya)}
+            { name: 'MERT', ...calculateDaysLeft(mert) },
+            { name: 'DEMIR', ...calculateDaysLeft(demir) },
+            { name: 'ELGORMUS', ...calculateDaysLeft(elgormus) },
+            { name: 'ALMIRA BIRTHDAY', ...calculateDaysLeft(tanya) }
         ];
     };
 
@@ -50,8 +47,8 @@ export default function Home() {
         <div className={styles.container}>
             <div>
                 {countdownData.map((item) => (
-                    <div key={item.name} style={{marginBottom: '20px', fontFamily: 'monospace'}}>
-                        <h2 style={{fontSize: '24px', margin: '0', fontWeight: 'normal'}}>{item.name}</h2>
+                    <div key={item.name} style={{ marginBottom: '20px', fontFamily: 'monospace' }}>
+                        <h2 style={{ fontSize: '24px', margin: '0', fontWeight: 'normal' }}>{item.name}</h2>
                         <h1 style={{
                             fontSize: '36px',
                             fontWeight: 'bold',
@@ -62,6 +59,21 @@ export default function Home() {
                         </h1>
                     </div>
                 ))}
+            </div>
+            <div style={{
+                position: 'fixed',
+                bottom: '20px',
+                right: '20px',
+                backgroundColor: '#333333', // Koyu arka plan
+                padding: '10px',
+                borderRadius: '8px',
+                boxShadow: '0 2px 4px rgba(0,0,0,0.5)', // Daha yumuşak gölge
+                color: '#ffffff', // Metin rengi beyaz
+                fontFamily: 'monospace'
+            }}>
+                <h4 style={{ margin: '0', fontSize: '16px', fontWeight: 'bold' }}>Examples of the number of days between fingerprint submission and ILR result.:</h4>
+                <p style={{ margin: '0', fontSize: '14px' }}>1st Example: Fingerprint Date: 06/09/2024 | ILR Decision Date: 04/12/2024 | Total Days Passed: 89</p>
+                <p style={{ margin: '0', fontSize: '14px' }}>2nd Example: Fingerprint Date: 19/09/2024 | ILR Decision Date: 14/12/2024 | Total Days Passed: 86</p>
             </div>
         </div>
     );
