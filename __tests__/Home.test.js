d0bf46b0a3a104e8fb039ee8fa38a2ab16fbe768
Ozgur Demir <ozgur.d@freelancer.run>
2025-07-22 22:26:24 +0100

    update

---
+++ __tests__/Home.test.js
@@ -0,0 +1,11 @@
+import {render, screen} from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Home page', () => {
+    it('renders the button with correct label', () => {
+        render(<Home/>)
+        expect(
+            screen.getByRole('button', {name: /okudum, anladım/i})
+        ).toBeInTheDocument()
+    })
+})
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    testEnvironment: 'jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
